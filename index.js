8c0b2c7dda4811b2158aceea37e9ab3b6a33ee3b
christian zambrana <chrislajas@gmail.com>
2019-05-25 19:28:54 +0000

    Automatically backed up by Learn

--- index.js
+++ index.js
@@ -1,7 +1,13 @@
 // write your code below!
-var name = "Joe"
+var name = "Susan"
 var height = 74
+describe("Height", () => {
+    it("is less than 40", () => {
+      expect(index.height).toBeLessThan(40)
+    })
+  })
 var message = `${name} is ${height} inches tall`
+
 
 module.exports = {name, height, message}
 console.log(message)
